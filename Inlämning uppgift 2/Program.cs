982532035f6518a97168a92ecc69f0bd5b9654d9
karamTahhan <kata07001@utb.vaxjo.se>
2024-09-12 14:18:06 +0200

    Add project files.

---
+++ Inlämning uppgift 2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Upg_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            Console.WriteLine("Your PC has crashed");
+        }
+    }
+}
